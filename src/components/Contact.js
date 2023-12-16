9d56cfec55cdbce4bfd3419e60a96a4f03a13d17
Courtney Caputo <courtneycaputo@Courtneys-MacBook-Air.local>
2023-12-16 10:42:04 -0800

    initial add

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+// components/Contact.js
+
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <h1>Contact</h1>
+      <p>Placeholder text for the contact section.</p>
+    </section>
+  );
+}
+
+export default Contact;
